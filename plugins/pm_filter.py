b9415f0b04736d30a834a6cb561c93f45f40b54f
Nothing <109523291+PrinceP61@users.noreply.github.com>
2022-07-19 22:14:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -451,20 +451,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
-        )
-
-    elif query.data == "imdb":
-        buttons = [[
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='help2')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.IMDB_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-
-    elif query.data == "help3":
+         )
+        elif query.data == "help3":
         buttons = [[
             InlineKeyboardButton('Info👀', callback_data='about')
         ],[
