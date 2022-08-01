0be70f5cea021eed747b6ab133b0fb4d364aebd3
Nothing <109523291+PrinceP61@users.noreply.github.com>
2022-08-01 15:11:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,7 +73,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#{file.file_id}',
+                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#https://droplink.co/api?api=a919f67b24025ccc3c2b02745435145e1f738a4d&url={file.file_id}',
                 ),
             ]
             for file in files
