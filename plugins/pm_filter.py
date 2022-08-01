5d20eb95f079e73a5aad346382e817fd314787b0
Nothing <109523291+PrinceP61@users.noreply.github.com>
2022-08-01 10:29:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -830,7 +830,6 @@ async def advantage_spell_chok(msg):
     if not g_s:
         k = await msg.reply("<b>ਮੈਨੂੰ ਇਸ ਨਾਮ ਦੀ ਕੋਈ ਫ਼ਿਲਮ ਨਹੀਂ ਮਿਲੀ..\n (ɪ ᴄᴏᴜʟᴅɴ'ᴛ ғɪɴᴅ ᴀɴʏ ᴍᴏᴠɪᴇ ɪɴ ᴛʜᴀᴛ ɴᴀᴍᴇ..)</b>")
         await asyncio.sleep(8)
-        await message.delete()
         await k.delete()
         return
     regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
@@ -860,7 +859,6 @@ async def advantage_spell_chok(msg):
     if not movielist:
         k = await msg.reply("<b>ਮੈਨੂੰ ਇਸ ਨਾਲ ਸਬੰਧਤ ਕੁਝ ਨਹੀਂ ਮਿਲਿਆ। ਆਪਣੇ ਸਪੈਲਿੰਗ ਦੀ ਜਾਂਚ ਕਰੋ..\n (ɪ ᴄᴏᴜʟᴅɴ'ᴛ ғɪɴᴅ ᴀɴʏᴛʜɪɴɢ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ᴛʜᴀᴛ. ᴄʜᴇᴄᴋ ʏᴏᴜʀ sᴘᴇʟʟɪɴɢ..)</b>")
         await asyncio.sleep(8)
-        await message.delete()
         await k.delete()
         return
     SPELL_CHECK[msg.message_id] = movielist
@@ -874,7 +872,6 @@ async def advantage_spell_chok(msg):
     pb = await msg.reply("<b>ਮੈਨੂੰ ਇਸ ਨਾਲ ਸੰਬੰਧਿਤ ਕੁਝ ਨਹੀਂ ਮਿਲਿਆ, ਕੀ ਤੁਹਾਡਾ ਮਤਲਬ ਇਹਨਾਂ ਵਿੱਚੋਂ ਕੋਈ ਇੱਕ ਸੀ?☼☺\n.( ɪ ᴄᴏᴜʟᴅɴ'ᴛ ғɪɴᴅ ᴀɴʏᴛʜɪɴɢ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ᴛʜᴀᴛ,ᴅɪᴅ ʏᴏᴜ ᴍᴇᴀɴ ᴀɴʏ ᴏɴᴇ ᴏғ ᴛʜᴇsᴇ?☼☺)</b>",
                     reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(8)
-    await message.delete()
     await pb.delete()
 
 async def manual_filters(client, message, text=False):
