71fbf8a105e2123dce67146d52812dec7cbf832b
Nothing <109523291+PrinceP61@users.noreply.github.com>
2022-08-01 15:12:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -709,7 +709,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#https://droplink.co/api?api=a919f67b24025ccc3c2b02745435145e1f738a4d&url={file.file_id}'
                 )
             ]
             for file in files
@@ -718,7 +718,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{get_size(file.file_size)}!{file.file_name}", callback_data=f'files#https://droplink.co/api?api=a919f67b24025ccc3c2b02745435145e1f738a4d&url={file.file_id}'
                 )
             ]
             for file in files
