3749b7a492ac085c261309544b974d1123e24a52
Nothing <109523291+PrinceP61@users.noreply.github.com>
2022-08-18 16:19:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -718,7 +718,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}⇥{file.file_name}", callback_data=url=f'files#{file.file_id}'
+                    text=f"{get_size(file.file_size)}⇥{file.file_name}", callback_data=f'files#{file.file_id}'
                 )
             ]
             for file in files
